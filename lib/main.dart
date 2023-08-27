8169fb18f3f05081453da98b4fb5f8e5ce2f6944
mahadiktejas2003 <mahadiktejas165@gmail.com>
2023-08-28 04:17:16 +0530

    uploading 2nd folder of flutter

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'home_page.dart';
 
 import 'chat_screen.dart';
+import 'package:chatapp/chat_screen.dart';
 
 void main() => runApp(new MyApp());
 
@@ -9,6 +10,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return new MaterialApp(
+      debugShowCheckedModeBanner: false,
       title: "Chat App",
       home: new HomePage(),
     );
